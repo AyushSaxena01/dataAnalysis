80ac0892e3943a12db3b0c2a73541923e5312718
Ayush Saxena <ayushsaxena0399@gmail.com>
2023-10-26 14:37:01 +0530

    README and completed task

---
+++ README.txt
@@ -0,0 +1,5 @@
+Data Analysis assignment
+
+=> Using fetch() with GET method to retrieve the data
+=> Using fetch() with POST method to send the required data after processing 
+=> Displaying the response from the server on terminal
--- app.js
+++ app.js
@@ -1,94 +1,101 @@
 const dataAnalysis = async () => {
-
-const fetchData = async ()=>{
-    
+  const fetchData = async () => {
     let response;
     let id;
     let data;
 
-    try{
-        response = await fetch('https://one00x-data-analysis.onrender.com/assignment?email=ayushsaxena0399@gmail.com');
-        id = response.headers.get('x-assignment-id');
-        data = await response.json();
-    } catch(error){
-        console.log(`Error occured in fetchData(): ${error}`);
+    try {
+      response = await fetch(
+        "https://one00x-data-analysis.onrender.com/assignment?email=ayushsaxena0399@gmail.com"
+      );
+      id = response.headers.get("x-assignment-id"); // assignment_id
+      data = await response.json(); // data - array of phrases
+    } catch (error) {
+      console.log(`Error occured in fetchData(): ${error}`);
     }
 
-return [id, data];
-   
-}
+    return [id, data]; // fetch() returns an array containing the assignment_id and data
+  };
 
-const dataArray = await fetchData();
-let assignment_id = dataArray[0]
-// console.log(assignment_id);
-let count=0;
+  const dataArray = await fetchData(); // storing the returned array in dataArray
+  const assignment_id = dataArray[0];  // storing the assignment_id
+  let count = 0;
 
-const processData = (data) => {
-   let array = data.map((element)=>{
-        count=0;
-        data.forEach((item) => {
-            if(element===item){
-                count++;
-            }
-        });
-        return count;
+  // function processData() - takes the dataArray for processing. 
+  // array frequencyCount - contains the frequency count of each element 
+  // const maximum - containts the max frequency count
+  // array frequent - containts all the sorted elements with highest frequency (repeated elements too)
+  // array words - contains unique words with highest frequency 
+
+  const processData = (data) => {
+    let frequencyCount = data.map((element) => {
+      count = 0;
+      data.forEach((item) => {
+        if (element === item) {
+          count++;
+        }
+      });
+      return count;
     });
 
-    const maximum = Math.max(...array);
-    
-    let frequent=[];
+    const maximum = Math.max(...frequencyCount);
 
-    array.forEach((element,i)=>{
-        if(element===maximum){
-            frequent.push(data[i]);
-        }
-    })
+    let frequent = [];
+
+    frequencyCount.forEach((element, i) => {
+      if (element === maximum) {
+        frequent.push(data[i]);
+      }
+    });
     frequent.sort();
 
-let words =[];
+    let words = [];
 
-for(i=0;i<frequent.length-2;i++){
-    if(words.length===0){
+    for (i = 0; i < frequent.length - 2; i++) {
+      if (words.length === 0) {
         words.push(frequent[0]);
+      }
+      if (frequent[i] !== frequent[i + 1]) {
+        words.push(frequent[i + 1]);
+      }
     }
-    if(frequent[i]!==frequent[i+1]){
-        words.push(frequent[i+1]);
-    }
-}
-return words;
+    return words;
+  };
 
- }
+  const frequentWords = processData(dataArray[1]); // frequentWords contains the returned highest frequency words array 
 
- const frequentWords = processData(dataArray[1]);
-//  console.log(frequentWords);
-
-const postData = async () => {
-
+  // postData() - takes answer as argument and post the data on required url, returns the response received
+  const postData = async (answer) => {
     let responsePost;
     let dataPost;
-try{
-    responsePost = await fetch('',{
-        method:'POST',
-        headers:{'Content-Type': 'application/json'},
-        body:JSON.stringify({'post':{'assignment_id':assignment_id, 'answer':''},}),
-    }); 
+    try {
+      responsePost = await fetch(
+        "https://one00x-data-analysis.onrender.com/assignment",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            assignment_id: assignment_id,
+            answer: answer,
+          }),
+        }
+      );
 
-    dataPost = await responsePost.json(); 
-    
-   } catch(error){
-    console.log(`Error in postData: ${error} `);
-   }
-return dataPost;
-}
+      dataPost = await responsePost.json();
+    } catch (error) {
+      console.log(`Error in postData: ${error} `);
+    }
+    return dataPost;
+  };
 
-}
+  let responseData;
+
+  // using for loop to post as two requests are to be made
+  for (i = 0; i < frequentWords.length; i++) {
+    responseData = await postData(frequentWords[i]);
+    console.log(`Response for '${frequentWords[i]}':`);
+    console.log(responseData);
+  }
+};
 
 dataAnalysis();
-
-
- 
-
-
-
-
-
